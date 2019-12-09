509f32297a7e8406cd841d0ea2495829481b7f0c
gabrielolanday <gco218@nyu.edu>
2019-12-09 12:53:27 -0500

    final

---
+++ script.js
@@ -0,0 +1,24 @@
+ScrollReveal({ reset: true });
+ScrollReveal({ delay: 0 });
+ScrollReveal({ duration: 1000 });
+ScrollReveal({ distance: '30px' });
+
+ScrollReveal().reveal('#home h1', {delay: 400});
+ScrollReveal().reveal('#home p', {delay: 800});
+
+ScrollReveal().reveal('#model iframe');
+ScrollReveal().reveal('#model p', {delay: 200});
+ScrollReveal().reveal('.modelcontainer iframe');
+ScrollReveal().reveal('.modelcontainer p', {delay: 200});
+
+
+ScrollReveal().reveal('#mograf h1');
+ScrollReveal().reveal('#mograf p', {delay: 200});
+ScrollReveal().reveal('#mograf iframe', {delay: 200});
+
+
+ScrollReveal().reveal('#stream h1');
+ScrollReveal().reveal('#stream iframe', {delay: 200});
+
+ScrollReveal().reveal('#about h1', {delay: 400});
+ScrollReveal().reveal('#about p', {delay: 800});
